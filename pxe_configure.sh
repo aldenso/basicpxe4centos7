f576abee25cfb2f932db0a17f1cf1c5b72d5d3d7
Aldo Sotolongo <aldenso@gmail.com>
2016-09-01 14:53:38 -0400

    added extras checks, included install of xinetd to avoid some failures, added replace for IP in menu, added replace some broadcast-address in template, added ipcalc for find net and broadcast

--- pxe_configure.sh
+++ pxe_configure.sh
@@ -1,11 +1,13 @@
-#!/bin/bash - 
-#===============================================================================
-#          FILE: pxe_configure.sh
-#         USAGE: ./pxe_configure.sh 
-#   DESCRIPTION: Script to automate a basic centos7 pxe
-#  REQUIREMENTS: centos7 OS
-#        AUTHOR: (aldenso), aldenso@gmail.com
-#===============================================================================
+#!/usr/bin/bash
+# @Author: Aldo Sotolongo
+# @Date:   2016-08-29T23:38:02-04:00
+# @Email:  aldenso@gmail.com
+# @Last modified by:   Aldo Sotolongo
+# @Last modified time: 2016-09-01T14:44:56-04:00
+# FILE: pxe_configure.sh
+# DESCRIPTION: Script to automate a basic centos7 pxe
+# REQUIREMENTS: centos7 OS, centos install image in $IMAGEDIR dir.
+
 ##############################
 # Configuration
 ##############################
@@ -15,24 +17,36 @@ IMAGEPXE="$IMAGEDIR/images/pxeboot"
 DHCPDIR="/etc/dhcp"
 TFTPCONF="/etc/xinetd.d"
 DHCPSERVER="192.168.100.1"
-DHCPSUBNET="192.168.100.0"
 DHCPSUBNETMASK="255.255.255.0"
+DHCPSUBNET=$(ipcalc -n $DHCPSERVER $DHCPSUBNETMASK | cut -d "=" -f2)
+DHCPBROADCAST=$(ipcalc -b $DHCPSERVER $DHCPSUBNETMASK | cut -d "=" -f2)
 DHCPRANGEFIRSTIP="192.168.100.150"
 DHCPRANGELASTIP="192.168.100.200"
 SYSLINUXDIR="/usr/share/syslinux"
 
 
+if ( [ -d "$IMAGEDIR" ] && [ -d "$IMAGEPXE" ] )
+then
+	echo "$IMAGEDIR and $IMAGEPXE exist"
+else
+	echo "Make sure IMAGEDIR and IMAGEPXE exist and are properly configured.
+IMAGEDIR: $IMAGEDIR
+IMAGEPXE: $IMAGEPXE
+"
+	exit 1
+fi
+
 ##############################
 # Install packages.
 ##############################
-packages="dhcp syslinux tftp-server httpd"
-yum install $packages -y > /dev/null 2>&1
+packages="dhcp syslinux tftp-server httpd xinetd"
+yum install -y $packages > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
 	echo "Packages $packages installed successfully"
 else
 	echo "Failed to install Packages."
-	exit 0
+	exit 1
 fi
 
 ##############################
@@ -45,20 +59,26 @@ cp tftp $TFTPCONF/tftp
 ##############################
 cp $DHCPDIR/dhcpd.conf $DHCPDIR/dhcpd.conf_$(date +%F-%T)
 cp dhcpd.conf-template dhcpd.conf-template-orig
-sed s/"subnet 192.168.100.0 netmask 255.255.255.0"/"subnet $DHCPSUBNET netmask $DHCPSUBNETMASK"/g dhcpd.conf-template > dhcpd.conf-tmp
-mv dhcpd.conf-tmp dhcpd.conf-template
-sed s/"range 192.168.100.231 192.168.100.253;"/"range $DHCPRANGEFIRSTIP $DHCPRANGELASTIP;"/g dhcpd.conf-template > dhcpd.conf-tmp
-mv dhcpd.conf-tmp dhcpd.conf-template
-sed s/"option routers 192.168.100.1;"/"option routers $DHCPSERVER;"/g dhcpd.conf-template > dhcpd.conf-tmp
-mv dhcpd.conf-tmp dhcpd.conf-template
-sed s/"next-server 192.168.100.1;"/"next-server $DHCPSERVER;"/g dhcpd.conf-template > dhcpd.conf-tmp
-mv dhcpd.conf-tmp dhcpd.conf-template
+sed -i s/"subnet 192.168.100.0 netmask 255.255.255.0"/"subnet $DHCPSUBNET netmask $DHCPSUBNETMASK"/g dhcpd.conf-template
+sed -i s/"range 192.168.100.231 192.168.100.253;"/"range $DHCPRANGEFIRSTIP $DHCPRANGELASTIP;"/g dhcpd.conf-template
+sed -i s/"option routers 192.168.100.1;"/"option routers $DHCPSERVER;"/g dhcpd.conf-template
+sed -i s/"next-server 192.168.100.1;"/"next-server $DHCPSERVER;"/g dhcpd.conf-template
+sed -i s/"option broadcast-address 192.168.100.254;"/"option broadcast-address $DHCPBROADCAST;"/g dhcpd.conf-template
 mv dhcpd.conf-template dhcpd.conf-template-final
 mv dhcpd.conf-template-orig dhcpd.conf-template
 cp dhcpd.conf-template-final $DHCPDIR/dhcpd.conf
 echo "###############################
 DHCP configured.
+DHCP IP: $DHCPSERVER
+DHCP MASK: $DHCPSUBNETMASK
+DHCP BROADCAST: $DHCPBROADCAST
+DHCP RANGE: $DHCPRANGEFIRSTIP - $DHCPRANGELASTIP
 ###############################"
+
+##############################
+# update syslinux menu
+##############################
+sed -e "s|inst.repo=http://192.168.100.1|inst.repo=http://$DHCPSERVER|g" default_menu > default_menu-final
 
 ##############################
 # copy syslinux templates
@@ -66,7 +86,7 @@ DHCP configured.
 cp $SYSLINUXDIR/menu.c32 $SYSLINUXDIR/pxelinux.0 $SYSLINUXDIR/memdisk $SYSLINUXDIR/mboot.c32 $SYSLINUXDIR/chain.c32 $TFTPDIR
 cp $IMAGEPXE/vmlinuz $IMAGEPXE/initrd.img $TFTPDIR
 mkdir $TFTPDIR/pxelinux.cfg
-cp default_menu $TFTPDIR/pxelinux.cfg/default
+cp default_menu-final $TFTPDIR/pxelinux.cfg/default
 echo "###############################
 TFTPBOOT configured.
 ###############################"
@@ -80,12 +100,14 @@ firewall-cmd --add-service=dhcp --permanent --zone=public > /dev/null 2>&1
 firewall-cmd --reload > /dev/null 2>&1
 echo "###############################
 Firewall configured.
+$(firewall-cmd --list-services --zone=public --permanent)
 ###############################"
 
 ##############################
 # change selinux to permissive
 ##############################
 setenforce 0
+sed -i s/"^SELINUX=enforcing"/"SELINUX=permissive"/g /etc/selinux/config
 echo "###############################
 SElinux changed to permissive.
 ###############################"
